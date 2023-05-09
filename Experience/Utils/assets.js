56c8e5fb69ecba138b03272e81ff30f49cbaa64b
echen12 <chen.edmond13@gmail.com>
2023-05-08 20:44:08 -0700

    initial commit

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/edmond_room.glb",
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/lofi_girl_study.mp4",
+    },
+];
